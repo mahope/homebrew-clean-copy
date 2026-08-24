9dc2dd605552009cf6eb5b4aa89355dc1e44cb67
Mads Holst Jensen <mads@mahope.dk>
2026-08-24 20:23:11 +0200

    clean-copy 1.4.6

--- Formula/clean-copy.rb
+++ Formula/clean-copy.rb
@@ -1,9 +1,9 @@
 class CleanCopy < Formula
   desc "Copy/paste text as clean Markdown from your terminal"
   homepage "https://github.com/mahope/clean-copy-cli"
-  url "https://github.com/mahope/clean-copy-cli/releases/download/v1.4.5/clean-copy-1.4.5.tar.gz"
-  version "1.4.4"
-  sha256 "5df723cd8337f648433239435e98a622666774627fa4ebf0b38bbc10be424003"
+  url "https://github.com/mahope/clean-copy-cli/releases/download/v1.4.6/clean-copy-1.4.6.tar.gz"
+  version "1.4.6"
+  sha256 "bc2990f9dbe1ca040ae0e8ae756f04cb211a19e33ac168c221ec179f9312b00d"
 
   depends_on "node"
 
