f6e340c7b217a72abdaabff3cc8c50cd9a4b6859
Mads Holst Jensen <mads@mahope.dk>
2026-08-25 11:53:49 +0200

    clean-copy-cli 1.5.0: CSV mode (-v/--csv)

--- Formula/clean-copy.rb
+++ Formula/clean-copy.rb
@@ -1,9 +1,9 @@
 class CleanCopy < Formula
   desc "Copy/paste text as clean Markdown from your terminal"
   homepage "https://github.com/mahope/clean-copy-cli"
-  url "https://github.com/mahope/clean-copy-cli/releases/download/v1.4.6/clean-copy-1.4.6.tar.gz"
-  version "1.4.6"
-  sha256 "2c45b10db97df08bff99dad1edaa6a068f7dc83f69c73ceefb61b028a4ef9dfd"
+  url "https://github.com/mahope/clean-copy-cli/releases/download/v1.5.0/clean-copy-1.5.0.tar.gz"
+  version "1.5.0"
+  sha256 "cec65fd19a43ace08031337fa4193f4434c41f0b3a8a3b4dcb0c75f55a4d96e7"
 
   depends_on "node"
 
